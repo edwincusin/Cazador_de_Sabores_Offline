ca32a7ba2b8629f25089cad484f28b8ea507a397
EDWINCSN <c.edwin32@hotmail.com>
2026-09-25 00:16:34 -0500

    feat-->PASO 3: El Modelo de la Base de Datos (db.ts)

---
+++ src/database/db.ts
@@ -0,0 +1,22 @@
+import *as SQLite from "expo-sqlite";
+
+//FUNCION PARA INICIALIZAR LA BASE DE DATOS CREAR O INICIAR
+export const initDatabase = async () => {
+
+    const db = await SQLite.openDatabaseAsync('cazador.bd');
+
+    await db.execAsync(
+        `
+        PRAGMA journal_mode =WAL;
+        CREATE TABLE IF NOT EXISTS registros(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            calificacion INTEGER NOT NULL,
+            comentarios TEXT NOT NULL,
+            fotoBase64 TEXT NOT NULL,
+            fecha TEXT NOT NULL
+        );
+        `
+    );
+    console.log("Base de datos inicializada con exito!!")
+}
